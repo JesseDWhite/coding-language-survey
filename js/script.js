8bcbce09bf0edf5a31cd571fb955f75592737adb
Jesse White <jesse.white6@gmail.com>
2021-03-26 16:25:40 -0700

    add name field that will populate in the answers.

--- js/script.js
+++ js/script.js
@@ -17,23 +17,24 @@ $(document).ready(function () {
         const q8 = parseInt($('#question8').val());
         const q9 = parseInt($('#question9').val());
         const q10 = parseInt($('#question10').val());
+        const name = $('#name').val()
         const result = add(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10);
         $("#survey-result").text(result);
 
         if (result < 20 && result > 9) {
-            return $("#survey-result").text("You are a glutton for punishment. You should learn Javascript!")
+            return $("#survey-result").text(`You are a glutton for punishment ${name}. You should learn Javascript!`)
         }
         else if (result >= 20 && result < 30) {
-            return $("#survey-result").text("Your name is clearly Bill Gates. You should learn C#!")
+            return $("#survey-result").text(`Your name is clearly Bill and not ${name}. You should learn C#!`)
         } else if (result >= 30 && result < 40) {
-            return $("#survey-result").text("You've heard of a computer. You should learn Python!")
+            return $("#survey-result").text(`You've heard of a computer ${name}. You should learn Python!`)
         } else if (result >= 40 && result < 50) {
-            return $("#survey-result").text("You have a relative with an app idea that's really dumb. You should learn Ruby!")
+            return $("#survey-result").text(`You have a relative with an app idea that's really dumb. You should learn Ruby ${name}!`)
         } else if (result > 50) {
             return $("#survey-result").text("You are cutting edge for your age. You should learn Rust!")
         }
         else {
-            return $("#survey-result").text("PLEASE ANSWER ALL QUESTIONS BEFORE SUBMITTING!")
+            return $("#survey-result").text(`Please answer more questions before submitting ${name}!`)
         }
     })
     $("#result").click(function () {
