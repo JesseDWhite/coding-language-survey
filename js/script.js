dedd6c4c265aecac82e67cee440979524f979934
Jesse White <jesse.white6@gmail.com>
2021-03-26 12:08:44 -0700

    WIP: still working on .show() and .hide().

--- js/script.js
+++ js/script.js
@@ -18,33 +18,25 @@ $(document).ready(function () {
         const q9 = parseInt($('#question9').val());
         const q10 = parseInt($('#question10').val());
         const result = add(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10);
-        $("#survey-result-showing").text(result);
+        $("#survey-result").text(result);
 
         if (result < 20 && result > 9) {
-            return $("#survey-result-showing").text("You are a glutton for punishment. You should learn Javascript!")
+            return $("#survey-result").text("You are a glutton for punishment. You should learn Javascript!")
         }
-        else if (result >= 20 && result < 40) {
-            return $("#survey-result-showing").text("Your name is clearly Bill Gates. You should learn C#!")
-        } else if (result >= 40 && result < 60) {
-            return $("#survey-result-showingt").text("You've heard of a computer. You should learn Python!")
-        } else if (result >= 60 && result < 80) {
-            return $("#survey-result-showing").text("You have a relative with an app idea that's really dumb. You should learn Ruby!")
-        } else if (result > 80) {
-            return $("#survey-result-showing").text("You are cutting edge for your age. You should learn Rust!")
+        else if (result >= 20 && result < 30) {
+            return $("#survey-result").text("Your name is clearly Bill Gates. You should learn C#!")
+        } else if (result >= 30 && result < 40) {
+            return $("#survey-result").text("You've heard of a computer. You should learn Python!")
+        } else if (result >= 40 && result < 50) {
+            return $("#survey-result").text("You have a relative with an app idea that's really dumb. You should learn Ruby!")
+        } else if (result > 50) {
+            return $("#survey-result").text("You are cutting edge for your age. You should learn Rust!")
         }
         else {
-            return $("#survey-result-showing").text("PLEASE ANSWER ALL QUESTIONS BEFORE SUBMITTING")
+            return $("#survey-result").text("PLEASE ANSWER ALL QUESTIONS BEFORE SUBMITTING!")
         }
     })
 })
 // $("#survey-result-showing").submit(function () {
 // $("#survey-result-showing").toggle();
 // $("#survey-result-hidden").toggle();
-
-// Ruby
-// C#
-// JavaScript
-// Go
-// Python
-// Rust
-// Swift
